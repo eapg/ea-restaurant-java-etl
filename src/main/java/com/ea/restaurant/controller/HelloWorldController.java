896e361da62c5f3989227bc59f8a45ee28c2f5dc
Elido P <elidopg@gmail.com>
2023-01-10 19:30:52 -0400

    adding sprint boot to java etl project

---
+++ src/main/java/com/ea/restaurant/controller/HelloWorldController.java
@@ -0,0 +1,11 @@
+package com.ea.restaurant.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloWorldController {
+    @GetMapping("/")
+    public String hello()
+    {
+        return "Hello World";
+    }
+}
