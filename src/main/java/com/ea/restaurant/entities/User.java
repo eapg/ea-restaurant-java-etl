6f3849cd769b81327f3e5c6adbf1071f09ecb515
Elido P <elidopg@gmail.com>
2023-01-13 10:26:30 -0400

    adding lombok, user and baseEntity to the project

---
+++ src/main/java/com/ea/restaurant/entities/User.java
@@ -0,0 +1,19 @@
+package com.ea.restaurant.entities;
+
+import lombok.*;
+
+@EqualsAndHashCode(callSuper = true)
+@Getter
+@Setter
+public class User extends BaseEntity {
+  private String name;
+  private String lastName;
+  private String username;
+  private String password;
+  private String roles;
+  private String type;
+
+  public User(long id) {
+    super(id);
+  }
+}
