7ce0319209b835ec057306963ed3bdf2bdf3aada
Elido P <elidopg@gmail.com>
2023-01-16 09:43:30 -0400

    mapping user entity with jpa

--- src/main/java/com/ea/restaurant/entities/BaseEntity.java
+++ src/main/java/com/ea/restaurant/entities/BaseEntity.java
@@ -1,6 +1,11 @@
 package com.ea.restaurant.entities;
 
 import com.ea.restaurant.constants.Status;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.NotNull;
 import java.time.Instant;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -9,16 +14,19 @@ import lombok.Setter;
 @EqualsAndHashCode(of = "id", callSuper = false)
 @Getter
 @Setter
+@MappedSuperclass
 public abstract class BaseEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
 
-  private long id;
-  private long updatedBy;
-  private long createdBy;
-  private Status entityStatus;
+  @NotNull private Long updatedBy;
+  @NotNull private Long createdBy;
+  @NotNull private Status entityStatus;
   private Instant createdDate;
   private Instant updatedDate;
 
-  public BaseEntity(long id) {
+  public BaseEntity(Long id) {
 
     this.id = id;
   }
--- src/main/java/com/ea/restaurant/entities/User.java
+++ src/main/java/com/ea/restaurant/entities/User.java
@@ -1,5 +1,8 @@
 package com.ea.restaurant.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,15 +10,18 @@ import lombok.Setter;
 @EqualsAndHashCode(callSuper = true)
 @Getter
 @Setter
+@Entity
+@Table(name = "users")
 public class User extends BaseEntity {
-  private String name;
-  private String lastName;
-  private String username;
-  private String password;
-  private String roles;
-  private String type;
 
-  public User(long id) {
+  @NotNull private String name;
+  @NotNull private String lastName;
+  @NotNull private String username;
+  @NotNull private String password;
+  @NotNull private String roles;
+  @NotNull private String type;
+
+  public User(Long id) {
     super(id);
   }
 }
