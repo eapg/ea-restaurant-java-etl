e07d681b415334144b028486c7e80d98d4c1fd2a
Elido P <elidopg@gmail.com>
2023-01-25 09:50:02 -0400

    adding user controller

--- src/test/java/com/ea/restaurant/service/impl/UserServiceImplTest.java
+++ src/test/java/com/ea/restaurant/service/impl/UserServiceImplTest.java
@@ -1,5 +1,7 @@
 package com.ea.restaurant.service.impl;
 
+import static com.ea.restaurant.service.impl.utils.fixtures.UserFixture.buildUser;
+
 import com.ea.restaurant.constants.Status;
 import com.ea.restaurant.entities.User;
 import com.ea.restaurant.repository.UserRepository;
@@ -27,17 +29,23 @@ class UserServiceImplTest {
 
   @Test
   public void whenUserCreate_ShouldCallRepositorySave() {
-    var user = new User(2L);
+    var user = buildUser();
+    var userReturned = buildUser(1L);
+    Mockito.when(this.userRepository.save(Mockito.eq(user))).thenReturn(userReturned);
     var userSaved = this.userService.create(user);
-    Mockito.verify(this.userRepository, Mockito.times(1)).save(user);
+    Mockito.verify(this.userRepository, Mockito.times(1)).save(Mockito.eq(user));
+    Assertions.assertEquals(userSaved, userReturned);
   }
 
   @Test
   public void whenFindUserById_ShouldCallRepositoryFindById() {
     var user = Optional.of(new User(2L));
-    Mockito.when(this.userRepository.findByIdAndEntityStatus(2L, Status.ACTIVE)).thenReturn(user);
+    Mockito.when(
+            this.userRepository.findByIdAndEntityStatus(Mockito.eq(2L), Mockito.eq(Status.ACTIVE)))
+        .thenReturn(user);
     var userReturned = this.userService.findEnabledById(2L);
-    Mockito.verify(userRepository, Mockito.times(1)).findByIdAndEntityStatus(2L, Status.ACTIVE);
+    Mockito.verify(userRepository, Mockito.times(1))
+        .findByIdAndEntityStatus(Mockito.eq(2L), Mockito.eq(Status.ACTIVE));
     Assertions.assertEquals(user, userReturned);
   }
 
@@ -45,11 +53,13 @@ class UserServiceImplTest {
   public void whenFindUserByUsername_ShouldCallRepositoryFindByUsername() {
     var user = Optional.of(new User(2L));
     user.get().setUsername("eap");
-    Mockito.when(this.userRepository.findByUsernameAndEntityStatus("eap", Status.ACTIVE))
+    Mockito.when(
+            this.userRepository.findByUsernameAndEntityStatus(
+                Mockito.eq("eap"), Mockito.eq(Status.ACTIVE)))
         .thenReturn(user);
     var userReturned = this.userService.findEnabledByUsername("eap");
     Mockito.verify(userRepository, Mockito.times(1))
-        .findByUsernameAndEntityStatus("eap", Status.ACTIVE);
+        .findByUsernameAndEntityStatus(Mockito.eq("eap"), Mockito.eq(Status.ACTIVE));
     Assertions.assertEquals(user, userReturned);
   }
 
@@ -60,9 +70,11 @@ class UserServiceImplTest {
     var user2 = new User(2L);
     users.add(user1);
     users.add(user2);
-    Mockito.when(this.userRepository.findAllByEntityStatus(Status.ACTIVE)).thenReturn(users);
+    Mockito.when(this.userRepository.findAllByEntityStatus(Mockito.eq(Status.ACTIVE)))
+        .thenReturn(users);
     var usersReturned = this.userService.findAllEnabled();
-    Mockito.verify(userRepository, Mockito.times(1)).findAllByEntityStatus(Status.ACTIVE);
+    Mockito.verify(userRepository, Mockito.times(1))
+        .findAllByEntityStatus(Mockito.eq(Status.ACTIVE));
     Assertions.assertEquals(usersReturned, users);
   }
 
@@ -70,10 +82,13 @@ class UserServiceImplTest {
   public void whenDeleteUserById_ShouldCallRepositoryDeleteById() {
     var userToDelete = new User(2L);
     var user = Optional.of(new User(2L));
-    Mockito.when(this.userRepository.findByIdAndEntityStatus(2L, Status.ACTIVE)).thenReturn(user);
+    Mockito.when(
+            this.userRepository.findByIdAndEntityStatus(Mockito.eq(2L), Mockito.eq(Status.ACTIVE)))
+        .thenReturn(user);
     this.userService.deleteUserById(2L, userToDelete);
-    Mockito.verify(userRepository, Mockito.times(1)).findByIdAndEntityStatus(2L, Status.ACTIVE);
-    Mockito.verify(userRepository, Mockito.times(1)).save(user.get());
+    Mockito.verify(userRepository, Mockito.times(1))
+        .findByIdAndEntityStatus(Mockito.eq(2L), Mockito.eq(Status.ACTIVE));
+    Mockito.verify(userRepository, Mockito.times(1)).save(Mockito.eq(user.get()));
   }
 
   @Test
@@ -82,9 +97,12 @@ class UserServiceImplTest {
     userToUpdate.setUsername("eapg");
     var user = Optional.of(new User(2L));
     user.get().setUsername("eap");
-    Mockito.when(this.userRepository.findByIdAndEntityStatus(2L, Status.ACTIVE)).thenReturn(user);
+    Mockito.when(
+            this.userRepository.findByIdAndEntityStatus(Mockito.eq(2L), Mockito.eq(Status.ACTIVE)))
+        .thenReturn(user);
     var userUpdated = this.userService.updateById(2L, userToUpdate);
-    Mockito.verify(userRepository, Mockito.times(1)).findByIdAndEntityStatus(2L, Status.ACTIVE);
+    Mockito.verify(userRepository, Mockito.times(1))
+        .findByIdAndEntityStatus(Mockito.eq(2L), Mockito.eq(Status.ACTIVE));
     Mockito.verify(userRepository, Mockito.times(1)).save(user.get());
     Assertions.assertEquals(userUpdated.getUsername(), "eapg");
   }
