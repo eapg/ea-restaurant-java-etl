0b74839b162bc8df82f4f2d471e11cccfc7cf6e8
Elido P <elidopg@gmail.com>
2023-06-15 14:14:22 -0400

    refactoring grpc mongo order status history mapper

--- src/main/java/com/ea/restaurant/util/GrpcMongoOrderStatusHistoryMapper.java
+++ src/main/java/com/ea/restaurant/util/GrpcMongoOrderStatusHistoryMapper.java
@@ -6,6 +6,7 @@ import com.ea.restaurant.constants.Status;
 import com.ea.restaurant.document.MongoOrderStatusHistory;
 import java.time.Instant;
 import java.util.List;
+import java.util.Optional;
 
 public class GrpcMongoOrderStatusHistoryMapper {
 
@@ -14,9 +15,19 @@ public class GrpcMongoOrderStatusHistoryMapper {
     return MongoOrderStatusHistory.builder()
         .orderId(mongoOrderStatusHistory.getOrderId())
         .fromStatus(OrderStatus.valueOf(mongoOrderStatusHistory.getFromStatus()))
-        .toStatus(OrderStatus.valueOf(mongoOrderStatusHistory.getToStatus()))
+        .toStatus(
+            Optional.ofNullable(mongoOrderStatusHistory)
+                .filter(com.ea.restaurant.grpc.MongoOrderStatusHistory::hasToStatus)
+                .map(mongoOrder -> mongoOrder.getToStatus().getValue())
+                .map(OrderStatus::valueOf)
+                .orElse(null))
         .fromTime(Instant.ofEpochMilli(mongoOrderStatusHistory.getFromTime()))
-        .toTime(Instant.ofEpochMilli(mongoOrderStatusHistory.getToTime()))
+        .toTime(
+            Optional.ofNullable(mongoOrderStatusHistory)
+                .filter(com.ea.restaurant.grpc.MongoOrderStatusHistory::hasToStatus)
+                .map(mongoOrder -> mongoOrder.getToTime().getValue())
+                .map(Instant::ofEpochMilli)
+                .orElse(null))
         .etlStatus(EtlStatus.valueOf(mongoOrderStatusHistory.getEtlStatus()))
         .entityStatus(Status.valueOf(mongoOrderStatusHistory.getEntityStatus()))
         .createdBy(mongoOrderStatusHistory.getCreatedBy())
